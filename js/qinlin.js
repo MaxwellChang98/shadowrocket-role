d2ff94772a29e24dca3f3692793be3fe84329483
张明亮 <zhangmingliang@yazuishou.com>
2023-10-23 18:08:54 +0800

    test

--- js/qinlin.js
+++ js/qinlin.js
@@ -22,11 +22,11 @@
  hostname = expire.vanemu.com
 
  *******************************/
-body = $response.body.replace("401", "200").replace(/\"group_is_pro":\w+/g, '\"group_is_pro":true')
+/*body = $response.body.replace("401", "200").replace(/\"group_is_pro":\w+/g, '\"group_is_pro":true')
 
-$done({body});
+$done({body});*/
 
-/*let obj = JSON.parse($response.body);
+let obj = JSON.parse($response.body);
 obj = {
     "data": {
         "effectiveStartTime": "2023-10-21 00:00:00",
@@ -38,4 +38,4 @@ obj = {
     "message": "success",
     "code": 0
 };
-$done({body: JSON.stringify(obj)});*/
+$done({body: JSON.stringify(obj)});
